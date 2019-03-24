ede55f4e336ea526f1da340ed5be1ed9face049e
Luis <laar96@unitec.edu>
2019-03-24 17:36:42 -0600

    gencode while

--- asm/for.asm
+++ asm/for.asm
@@ -4,13 +4,15 @@ extern printf
 extern scanf
 
 section .data
-str7 db "End of loop, ", 0
+n dd 10
+arr TIMES 10 DD 0
+str7 db "", 0
 str2 db "false", 0
 str1 db "true", 0
-str3 db "%s", 0
-str6 db "%s%d%c", 0
-str4 db "i = ", 0
-str5 db "%d%c", 0
+str3 db "%d", 0
+str4 db "%s", 0
+str5 db " ", 0
+str6 db "%s%c", 0
 
 section .text
 
@@ -19,14 +21,72 @@ push ebp
 mov ebp, esp
 sub esp, 20
 
-mov eax, 10
-mov dword[ebp - 4], eax
+mov eax, 24
+mov ecx, 0
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 71
+mov ecx, 1
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 94
+mov ecx, 2
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 3
+mov ecx, 3
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 46
+mov ecx, 4
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 54
+mov ecx, 5
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 96
+mov ecx, 6
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 14
+mov ecx, 7
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 7
+mov ecx, 8
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
+mov eax, 42
+mov ecx, 9
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+mov dword[edx], eax
 mov eax, 0
 mov dword[ebp - 4], eax
 for_label3:
 mov eax, dword[ebp - 4]
-cmp eax, 10
-jle label4
+cmp eax, dword[n]
+jl label4
 mov eax, 1
 jmp end_label4
 label4:
@@ -36,15 +96,18 @@ mov dword[ebp - 16], eax
 mov eax, dword[ebp - 16] ; for cond
 cmp eax, 1
 jge end_for_label3
-push str4
+mov ecx, dword[ebp - 4]
+shl ecx, 2
+mov edx, arr
+add edx, ecx
+push dword[edx]
 push str3
 call printf
 add esp, 8
-push 10
-push dword[ebp - 4]
 push str5
+push str4
 call printf
-add esp, 12
+add esp, 8
 mov eax, dword[ebp - 4]
 add eax, 1
 mov dword[ebp - 20], eax
@@ -54,11 +117,10 @@ jmp for_label3
 end_for_label3:
 
 push 10
-push dword[ebp - 4]
 push str7
 push str6
 call printf
-add esp, 16
+add esp, 12
 
 end_main:
 add esp, 20
