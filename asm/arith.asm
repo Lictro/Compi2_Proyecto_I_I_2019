20e2c10ef44351885d7bf99308b10898a8a7bd4f
Luis <laar96@unitec.edu>
2019-03-24 11:09:50 -0600

    gencode bitwise

--- asm/arith.asm
+++ asm/arith.asm
@@ -4,12 +4,15 @@ extern printf
 extern scanf
 
 section .data
+str11 dd "izquerda ", 0
+str10 dd "%s%d%c", 0
 str9 dd "menor igual ", 0
 str8 dd "mayor igual ", 0
 str7 dd "menor ", 0
 str2 dd "false", 0
 str1 dd "true", 0
 str3 dd "%s%s%c", 0
+str12 dd "derecha ", 0
 str5 dd "distinto ", 0
 str4 dd "igual ", 0
 str6 dd "mayor ", 0
@@ -19,7 +22,7 @@ section .text
 main:
 push ebp
 mov ebp, esp
-sub esp, 48
+sub esp, 64
 
 push 10
 mov eax, 10
@@ -30,8 +33,8 @@ jmp end_label13
 label13:
 mov eax, 0
 end_label13:
-mov dword[ebp - 28], eax
-mov eax, dword[ebp - 28]
+mov dword[ebp - 36], eax
+mov eax, dword[ebp - 36]
 cmp eax, 0
 je label14
 push str2
@@ -53,8 +56,8 @@ jmp end_label15
 label15:
 mov eax, 0
 end_label15:
-mov dword[ebp - 32], eax
-mov eax, dword[ebp - 32]
+mov dword[ebp - 40], eax
+mov eax, dword[ebp - 40]
 cmp eax, 0
 je label16
 push str2
@@ -76,8 +79,8 @@ jmp end_label17
 label17:
 mov eax, 0
 end_label17:
-mov dword[ebp - 36], eax
-mov eax, dword[ebp - 36]
+mov dword[ebp - 44], eax
+mov eax, dword[ebp - 44]
 cmp eax, 0
 je label18
 push str2
@@ -99,8 +102,8 @@ jmp end_label19
 label19:
 mov eax, 0
 end_label19:
-mov dword[ebp - 40], eax
-mov eax, dword[ebp - 40]
+mov dword[ebp - 48], eax
+mov eax, dword[ebp - 48]
 cmp eax, 0
 je label20
 push str2
@@ -122,8 +125,8 @@ jmp end_label21
 label21:
 mov eax, 0
 end_label21:
-mov dword[ebp - 44], eax
-mov eax, dword[ebp - 44]
+mov dword[ebp - 52], eax
+mov eax, dword[ebp - 52]
 cmp eax, 0
 je label22
 push str2
@@ -145,8 +148,8 @@ jmp end_label23
 label23:
 mov eax, 0
 end_label23:
-mov dword[ebp - 48], eax
-mov eax, dword[ebp - 48]
+mov dword[ebp - 56], eax
+mov eax, dword[ebp - 56]
 cmp eax, 0
 je label24
 push str2
@@ -159,9 +162,27 @@ push str9
 push str3
 call printf
 add esp, 16
+push 10
+mov eax, 100
+sal eax, 1
+mov dword[ebp - 60], eax
+push dword[ebp - 60]
+push str11
+push str10
+call printf
+add esp, 16
+push 10
+mov eax, 100
+sar eax, 1
+mov dword[ebp - 64], eax
+push dword[ebp - 64]
+push str12
+push str10
+call printf
+add esp, 16
 
 end_main:
-add esp, 48
+add esp, 64
 leave
 ret
 
